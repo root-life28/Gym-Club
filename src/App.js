6cadef4645d3979182bc798448dc632def73d021
Muzahidul <bekon28@outlook.com>
2022-09-24 11:07:51 +0600

    finished reason section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Hero  from './component/Hero/Hero';
 import Programs from './component/programs/Programs';
+import Reason from './component/Reasons/Reason';
 
 function App() {
   return (
     <div className="App">
         <Hero/>
         <Programs/>
+        <Reason/>
     </div>
   );
 }
