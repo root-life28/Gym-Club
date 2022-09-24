0a3aef94eec71cc50cbef12ae7990264fc53b7b9
Muzahidul <bekon28@outlook.com>
2022-09-24 10:03:45 +0600

    finished programs component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Hero  from './component/Hero';
+import Hero  from './component/Hero/Hero';
+import Programs from './component/programs/Programs';
 
 function App() {
   return (
     <div className="App">
         <Hero/>
+        <Programs/>
     </div>
   );
 }
