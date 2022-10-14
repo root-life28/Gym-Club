ac20aeabd40d80bf4f91e8ed0e5919da8ba3a603
Muzahidul <bekon28@outlook.com>
2022-10-14 12:23:50 +0600

    responsive and animation added

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
 import './Header.css'
 import Logo from '../../assets/logo.png'
+import Bars from '../../assets/bars.png'
+import { useState } from 'react'
+import { Link } from 'react-scroll'
 
 const Header = () => {
+  const mobile=window.innerWidth<=768?true: false
+  const [menuOpened,setMenuOpened]=useState(false);
   return (
     <div className="header">
         <img  src={Logo} alt="Logo" className='logo'  />
-        <ul className='header-menu'>
-            <li>Home</li>
-            <li>Programs</li>
-            <li>Why us</li>
-            <li>Plans</li>
-            <li>Testimonial</li>
-        </ul>
+        {(menuOpened===false&&mobile===true)?(
+          <div
+           style={{backgroundColor:'var(--appColor)',padding:'0.55rem',borderRadius:'5px'}}><img src={Bars} alt="bars" style={{width:'1.5rem',height:'1.5rem'}} 
+           onClick={()=>setMenuOpened(true)}
+           /></div>
+        ): <ul className='header-menu'>
+        <li ><Link onClick={()=>setMenuOpened(false)}
+        to='hero' className={true} smooth={true}>Home</Link></li>
+        <li ><Link onClick={()=>setMenuOpened(false)}
+        to='programs' className={true} smooth={true} >Programs</Link></li>
+        <li ><Link onClick={()=>setMenuOpened(false)}
+        to='reasons' className={true} smooth={true}>Why us</Link></li>
+        <li >
+          <Link onClick={()=>setMenuOpened(false)}
+        to='plans-container' className={true} smooth={true}
+          >Plans</Link></li>
+        <li><Link  onClick={()=>setMenuOpened(false)}
+        to='Testimonials' span={true} smooth={true}>Testimonial</Link></li>
+    </ul>}
+       
     </div>
   )
 }
--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -5,8 +5,11 @@ import hero_image from '../../assets/hero_image.png'
 import hero_image_back from '../../assets/hero_image_back.png'
 import Heart from '../../assets/heart.png'
 import Calories from '../../assets/calories.png'
+import {motion} from 'framer-motion'
 
  const Hero = () => {
+    const transition={type:'spring', duration:3}
+    const mobile=window.innerWidth<=768?true: false
   return (
     <div className="hero">
         <div className="blur hero-blur"></div>
@@ -14,7 +17,12 @@ import Calories from '../../assets/calories.png'
             <Header/>
             {/* Best ad */}
             <div className="the-best-ad">
-                <div></div>
+                <motion.div initial={{left:mobile?"165px":"238"}}
+                  whileInView={{left:'8px'}} transition={{...transition,type:'tween'}}>
+                    
+                   
+                    
+                </motion.div>
                 <span>the best fitness club in the town</span>
             </div>
 
@@ -52,22 +60,30 @@ import Calories from '../../assets/calories.png'
         </div>
         <div className="right-h">
             <button className="btn">Join Now</button>
-            <div className="heart-rate">
+            <motion.div className="heart-rate" initial={{right:'-1rem'}} whileInView={{right:'4rem'}} transition={transition}>
                 <img src={Heart} alt="Heart" />
                 <span>Heart Rate</span><span>116 bpm</span>
-            </div>
+            </motion.div>
             {/* hero images */}
             <img src={hero_image} className="hero-image" alt="hero_image"  />
-            <img src={hero_image_back} className="hero-image-back" alt="hero_image_back"  />
+            <motion.img 
+            initial={{right:'11rem'}}
+            whileInView={{right:"20rem"}}
+            transition={transition}
+            src={hero_image_back} className="hero-image-back" alt="hero_image_back"  />
 
             {/* calories */}
-            <div className="calories">
+            <motion.div initial={{right:"37rem"}}
+            whileInView={{right:"28rem"}}
+            transition={transition}
+
+             className="calories">
                 <img src={Calories} alt="Calories" />
                 <div>
                     <span>Calories Burned</span>
                     <span>220 kcal</span>
                 </div>
-            </div>
+            </motion.div>
         </div>
     </div>
   )
